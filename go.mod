63fac02028041f841f8a48c310e4a2e1cd1e8592
Andy Tan <Chien@tanqiendeMacBook-Pro.local>
2021-07-15 11:20:12 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module ToDo/server
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.10.2 // indirect
+	go.mongodb.org/mongo-driver v1.6.0
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"ToDo/server/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func main() {
+	r := router.Router()
+	// fs := http.FileServer(http.Dir("build"))
+	// http.Handle("/", fs)
+	fmt.Println("Starting server on the port 8080...")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
